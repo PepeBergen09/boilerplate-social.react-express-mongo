917b7c089efc15ca6b6ce3c18a6d5132af5d0783
Pepe Martinez <josedejesus.martinez@gmail.com>
2022-01-24 10:23:43 +0100

    run build for first mvp

---
+++ react-front/build/precache-manifest.043cdf50d6b8898fb3666b590d2137f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "528ccb98930a482f6f64",
+    "url": "/static/js/main.528ccb98.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "cf6aab8c49e0b1091ca4",
+    "url": "/static/js/2.cf6aab8c.chunk.js"
+  },
+  {
+    "revision": "b23d5960ea1aa29a34d567b62f9d1617",
+    "url": "/static/media/avatar.b23d5960.jpg"
+  },
+  {
+    "revision": "586806cd047e33b5ee7a43fc4252208d",
+    "url": "/static/media/mountains.586806cd.jpg"
+  },
+  {
+    "revision": "a78eb842eca27616547fddbb3dc76922",
+    "url": "/index.html"
+  }
+];
